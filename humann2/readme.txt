e9d5aa444cec3f863ad00e42494a3b09d24d870c
Hanna Berman <hannalberman@gmail.com>
2019-04-05 09:26:48 -0400

    Humann2 scripts

---
+++ humann2/readme.txt
@@ -0,0 +1,4 @@
+Profiling samples with Humann2 
+
+1) Concatenate paired reads and send to Humann2 software `./scripts/humann2.sh`
+2) Merge and normalize tables with `./scripts/humann2PostProcessing.sh`
---
+++ humann2/scripts/humann2PostProcessing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -o /home/hlberman/VMMG/humann2/humann2_%j.out
+#SBATCH -c 8
+
+accession=$1
+in_path=/home/hlberman/VMMG/humann2/output
+out_path=/home/hlberman/VMMG/humann2/merged_tables
+
+# 1) Join files
+humann2_join_tables -i $in_path -o $out_path/VM_genefamilies.tsv --file_name genefamilies
+humann2_join_tables -i $in_path -o $out_path/VM_pathabundance.tsv --file_name pathabundance
+humann2_join_tables -i $in_path -o $out_path/VM_pathcoverage.tsv --file_name pathcoverage
+
+# 2) Renormalize to relative abundance
+humann2_renorm_table -i $out_path/VM_genefamilies.tsv -o $out_path/VM_genefamilies_cpm.tsv --units cpm
+humann2_renorm_table -i $out_path/VM_pathabundance.tsv -o $out_path/VM_pathabundance_cpm.tsv --units cpm
+humann2_renorm_table -i $out_path/VM_pathcoverage.tsv  -o $out_path/VM_pathcoverage_cpm.tsv  --units cpm
+
