e78069a7141c8461c97f3f1cba392e533c565122
Hanna Berman <hannalberman@gmail.com>
2019-08-07 09:41:09 -0400

    updates on clade assignments analyses

--- gard_mapping/clade_assignments/brc_scripts/bam2fastq.sh
+++ gard_mapping/clade_assignments/brc_scripts/bam2fastq.sh
@@ -7,7 +7,19 @@ f_bam_path_42=/home/hlberman/VMMG/gard_map/filtered_out_42
 out_dir_20=/home/hlberman/VMMG/gard_map/filtered_fastq_20
 out_dir_42=/home/hlberman/VMMG/gard_map/filtered_fastq_42
 
-#files with mapq 20
+
+#files without mapq filter
+samtools sort \
+    -n \
+    -o $unf_bam_path/${accession}_FgardRef_n.bam \
+    $unf_bam_path/${accession}_FgardRef.bam
+samtools fastq \
+    -1 $out_dir_unf/${accession}_FgardRef_1.fastq.gz \
+    -2 $out_dir_unf/${accession}_FgardRef_2.fastq.gz \
+    -s $out_dir_unf/${accession}_FgardRef_S.fastq.gz \
+    $unf_bam_path/${accession}_FgardRef_n.bam
+
+# #files with mapq 20
 samtools sort \
     -n \
     -o $f_bam_path_20/${accession}_FgardRef_n.bam \
@@ -19,7 +31,7 @@ samtools fastq \
     $f_bam_path_20/${accession}_FgardRef_n.bam
 
 
-#files with mapq 42 (max)
+# #files with mapq 42 (max)
 samtools sort \
     -n \
     -o $f_bam_path_42/${accession}_FgardRef_n.bam \
